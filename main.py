2de99db6beca2f3d921bd4f6b59e906b46fac01d
Nikhil Raj <nikhilrajpandey1@gmail.com>
2019-09-19 18:32:47 +0530

    Make program to notify and adding settings.json to make program customizable

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 import requests #pip install requests
 from bs4 import BeautifulSoup #pip install bs4
 import pygame #pip install  pygame
-import time 
+import notify2 #pip install notify2
+import os
+import time
+import json
+
+with open('settings.json','r') as file:
+    settings = json.load(file)
+
+# initializing notify2
+notify2.init("Amazon Price Tracker")
 
 # To play a ding if the product is in our budget 
 pygame.mixer.init()
-pygame.mixer.music.load("ding.wav")
+pygame.mixer.music.load(settings["remind-sound-path"])
 
 # Set your budjet
-my_price = int(10000) 
+my_price = settings['budget']
 
 # initializing Currency Symbols to substract it from our string
 currency_symbols = ['€', '	£', '$', "¥", "HK$", "₹", "¥", "," ] 
 
 # the URL we are going to use
-URL = ('https://www.amazon.in/G731GT-Graphics-i5-9300H-Windows-G731GT-AU022T/dp/B07S36XJ8Q/ref=sr_1_3?crid=2T1YQNU9F8MMO&keywords=asus+rog+laptop&qid=1568612002&sprefix=Asus+%2Caps%2C685&sr=8-3')
+URL = settings['url']
 
 # Google "My User Agent" And Replace It
 headers = {"User-Agent": 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.75 Safari/537.36'} 
 
 def checking_price():
-
     page = requests.get(URL, headers=headers)
     soup  = BeautifulSoup(page.content, 'html.parser')
 
@@ -38,14 +46,21 @@ def checking_price():
 
     print("The Product Name is:" ,product_title.strip())
     print("The Price is:" ,product_price)
-    
+
+    # Making Alert
+    alert = notify2.Notification(product_title.strip(),message=f'Current Price: {product_price}', icon='')
+    alert.set_urgency(notify2.URGENCY_NORMAL)
+    alert.show()
+
     # checking the price
     if(product_price<my_price):
         pygame.mixer.music.play()
         print("You Can Buy This Now!")
+        time.sleep(3) # audio will be played first then exit the program. This time for audio playing.
+        exit()
     else:
         print("The Price Is Too High!")
 
 while True:
     checking_price()
-    time.sleep(3600) #It is set to run the program once in an hour! You can change by changing the value in seconds!
+    time.sleep(settings['remind-time']) #It is set to run the program once in an hour! You can change by changing the value in seconds!
